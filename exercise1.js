31f626d5a5c4e8ac427b6f65e661fd8b704c4219
shindo806 <thaisonlamsp@gmail.com>
2019-06-17 18:36:23 +0700

    new lesson

---
+++ exercise1.js
@@ -0,0 +1,26 @@
+/**
+ * Tạo 1 hàm doAfter nhận vào 2 tham số:
+ *  - Tham số thứ 1: 1 function
+ *  - Tham số thứ 2: Thời gian x (ms)
+ * Hàm này sẽ gọi function sau 1 khoảng thời gian x ms VÀ trả về 1 promise để có thể gọi như sau
+ */
+var doAfter = async function(func, ms) {
+  await setTimeout(func, ms);
+  return new Promise(function(resolve) {
+  return setTimeout(resolve, ms);
+  })
+ }
+
+function sayHello() {
+  console.log('Hello');
+}
+
+function sayGoodbye() {
+  console.log('Goodbye');
+}
+
+doAfter(sayHello, 1000).then(sayGoodbye);
+// Expect: 
+// Đợi 1s
+// Hello
+// Goodbye
